c380793e9d904a299992c8b879f0d5399ad4b137
Marvin Naessig <marvinabn@gmail.com>
2023-01-10 23:09:32 -0800

    le

--- test_api.py
+++ test_api.py
@@ -1,4 +1,22 @@
 # pylint: disable=missing-docstring,invalid-name
+import requests
 
-# TODO: paste the code from Kitt's instructions
+url = 'http://api.openweathermap.org/data/2.5/forecast?'
+response = requests.get(
+    url,
+    params = {
+        'lat': '44.34',
+        'lon': '10.99',
+        'appid': 'f5191f73c320e67df9461049016befe3'
+    }).json()
 
+temp = response['list'][0]['main']['temp']
+date = response['list'][0]['dt_txt']
+weather = response['list'][0]['weather'][0]['main']
+
+
+print(weather)
+
+print(temp)
+
+print(date)
